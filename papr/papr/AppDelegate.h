3b107d0985a539f0df496f31e5c483d2d2494ac7
Brian WF Tobin <brian@jebudas.com>
2017-07-05 08:03:07 -0400

    BRIAN . First Commit

---
+++ papr/papr/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  papr
+//
+//  Created by Brian WF Tobin on 7/5/17.
+//  Copyright © 2017 Jebudas Communications, Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ papr/papr/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  papr
+//
+//  Created by Brian WF Tobin on 7/5/17.
+//  Copyright © 2017 Jebudas Communications, Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
